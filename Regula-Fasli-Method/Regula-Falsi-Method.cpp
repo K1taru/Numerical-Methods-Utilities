edfa5608a5cac8fa1ad35c96faf2d1e605626c08
K1taru <jay.ablaza.garcia@gmail.com>
2025-02-21 15:02:09 +0800

    included user input

--- Regula-Fasli-Method/Regula-Falsi-Method.cpp
+++ Regula-Fasli-Method/Regula-Falsi-Method.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+double function(double x) {
+  return 3 * cos(-2 * x);
+}
+
+
 int main() {
-  cout << "Test1";
+  double a, b, Ex;
+
+  cout << "Enter a: ";
+  cin >> a;
+  cout << "Enter b: ";
+  cin >> b;
+  cout << "Enter error tolerance (Ex): ";
+  cin >> Ex;
 
   return 0;
 }
