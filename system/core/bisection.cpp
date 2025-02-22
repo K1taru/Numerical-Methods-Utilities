01354d429e33db664a35f3b67c092fa70960f33e
K1taru <jay.ablaza.garcia@gmail.com>
2025-02-23 00:40:26 +0800

    initial commit to system framework unified programs

---
+++ system/core/bisection.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double function(double x) {
+    return pow(x, 3) - 4;
+} // Example function: f(x) = x^3 - 4 (you can modify this function)
+
+double bisectionMethod(double a, double b, double Ex) { // error tolerance for f(c)
+    if (function(a) * function(b) >= 0) {
+        cout << "Invalid interval: f(a) and f(b) must have opposite signs or f(a) * f(b) < 0.\n";
+        return -1;
+    }
+
+    int iteration = 0;
+    double c;
+    while (true) { // loop until |f(c)| <= Ex
+        c = (a + b) / 2;
+        double f_a = function(a);
+        double f_b = function(b);
+        double f_c = function(c);
+
+        cout << std::setprecision(10);
+        cout << "Iteration: " << ++iteration << '\n';
+        cout << "  a = " << a << '\n';
+        cout << "  b = " << b << '\n';
+        cout << "  c = " << c << '\n';
+        cout << "  f(a) = " << f_a << '\n';
+        cout << "  f(b) = " << f_b << '\n';
+        cout << "  f(c) = " << f_c << "\n\n";
+
+        // |f(c)| is <= Ex
+        if (fabs(f_c) <= Ex)
+            break;
+
+        if (f_a * f_c < 0)
+            b = c;
+        else
+            a = c;
+    }
+    return c;
+}
---
+++ system/core/regula_falsi.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double function(double x) {
+  return 3 * cos(-2 * x);
+} // Example function in assignment
+
+double regulaFalsiMethod(double a, double b, double Ex) {
+  if (function(a) * function(b) >= 0) {
+    cout << "Invalid interval: f(a) and f(b) must have opposite signs or f(a) * f(b) < 0.\n";
+    return -1;
+  }
+  
+    int iteration = 0;
+    double x, F_a, F_b, F_x;
+
+    while (true) { // loop until |F(x)| <= Ex
+      F_a = function(a);
+      F_b = function(b);
+
+      x = (a * F_b - b * F_a) / (F_b - F_a);
+      F_x = function(x);
+
+      cout << std::setprecision(10);
+      cout << "Iteration: " << ++iteration << '\n';
+      cout << "  a = " << a << '\n';
+      cout << "  b = " << b << '\n';
+      cout << "  x = " << x << '\n';
+      cout << "  F(a) = " << F_a << '\n';
+      cout << "  F(b) = " << F_b << '\n';
+      cout << "  F(x) = " << F_x << "\n\n";
+
+      // Stop if |F(x)| <= Ex
+      if (fabs(F_x) <= Ex)
+          break;
+
+      // Update interval based on sign of function values
+      if (F_a * F_x < 0)
+          b = x;
+      else
+          a = x;
+  }
+  return x;
+}
