f667c38af902c9d3c7505f883b8641d411f6853b
K1taru <jay.ablaza.garcia@gmail.com>
2025-02-25 16:22:40 +0800

    removed header files from bisection and regula falsi method

--- bisection_method/bisection.h
+++
@@ -1,6 +0,0 @@
-#ifndef BISECTION_H
-#define BISECTION_H
-
-double bisectionMethod(double a, double b, double Ex);
-
-#endif
--- regula_falsi_method/regula_falsi.h
+++
@@ -1,6 +0,0 @@
-#ifndef REGULA_FALSI_H
-#define REGULA_FALSI_H
-
-double regulaFalsiMethod(double a, double b, double Ex);
-
-#endif
